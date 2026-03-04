7cfb4b288521e1f82b26c65b4743f432a60db797
iediot <eduardnicolae092@gmail.com>
2026-03-04 21:08:59 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tabirca Nicolae-Eduard on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
